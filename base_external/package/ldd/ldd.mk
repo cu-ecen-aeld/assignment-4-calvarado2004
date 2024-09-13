32110f678d8a8fcad7a3ccb1c416ba673dc21af7
Carlos Alvarado Martinez <carlos-alvarado@outlook.com>
2024-09-12 22:08:19 -0400

    assignment 7 part 2

---
+++ base_external/package/ldd/ldd.mk
@@ -0,0 +1,24 @@
+################################################################################
+#
+# ldd Makefile for Buildroot
+#
+################################################################################
+
+LDD_VERSION = 718cbdf
+LDD_SITE = 'git@github.com:cu-ecen-aeld/assignment-7-calvarado2004.git'
+LDD_INSTALL_TARGET = YES
+LDD_SITE_METHOD = git
+LDD_GIT_SUBMODULES = YES
+
+define LDD_BUILD_CMDS
+	$(MAKE) -C $(LDD_SITE) clean all
+endef
+
+define LDD_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0775 $(@D)/scull/scull_unload $(TARGET_DIR)/usr/bin
+endef
+
+$(eval $(generic-package))
