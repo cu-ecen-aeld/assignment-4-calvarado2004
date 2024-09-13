3380a0650f75db5c735f2594638e5a4e8f63620a
Carlos Alvarado Martinez <carlos-alvarado@outlook.com>
2024-09-12 23:00:59 -0400

    assignment 7 part 2

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -11,7 +11,7 @@ LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
 
 define LDD_BUILD_CMDS
-	$(MAKE) -C $(INSTALL) clean all
+	$(MAKE) -C $(TARGET_CC) clean all
 endef
 
 define LDD_INSTALL_TARGET_CMDS
