b919ac446b05658cc57df8c97a5fb4b5ba12e7a2
Carlos Alvarado Martinez <carlos-alvarado@outlook.com>
2024-09-14 00:08:38 -0400

    assignment 7 part 2

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -13,7 +13,17 @@ LDD_GIT_SUBMODULES = YES
 LDD_MODULE_SUBDIRS = scull
 LDD_MODULE_SUBDIRS += misc-modules
 
-LDD_MODULE_MAKE_OPTS = KERNELDIR=$(LINUX_DIR)
+define LDD_BUILD_CMDS
+    $(MAKE) CC="$(TARGET_CC)" -C $(@D)/scull all
+    $(MAKE) CC="$(TARGET_CC)" -C $(@D)/misc-modules all
+endef
+
+define LDD_INSTALL_TARGET_CMDS
+    $(INSTALL) -d 0755 $(TARGET_DIR)/lib/modules/$(LINUX_VERSION)
+    $(INSTALL) -m 0755 $(@D)/scull/scull.ko $(TARGET_DIR)/lib/modules/$(LINUX_VERSION)
+    $(INSTALL) -m 0755 $(@D)/misc-modules/faulty.ko $(TARGET_DIR)/lib/modules/$(LINUX_VERSION)
+    $(INSTALL) -m 0755 $(@D)/misc-modules/hello.ko $(TARGET_DIR)/lib/modules/$(LINUX_VERSION)
+endef
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
